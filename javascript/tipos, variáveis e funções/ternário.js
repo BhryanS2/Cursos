d77d1f3a30361a4b64a067997e73398f88233933
BhryanS2 <bhryanstepenhen@gmail.com>
2022-07-27 08:22:48 -0300

    ternário

---
+++ javascript/tipos, variáveis e funções/ternário.js
@@ -0,0 +1,12 @@
+// ternário
+const idadeMinima = 18;
+const idade = 16;
+
+// if (idade >= idadeMinima) {
+//   console.log("cerveja");
+// } else {
+//   console.log("água");
+// }
+
+const bebida = idade >= idadeMinima ? "cerveja" : "água";
+console.log(bebida); // água
