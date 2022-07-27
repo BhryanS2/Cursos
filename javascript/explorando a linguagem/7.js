c6e80d355b2d447bed2bcf991e86446adacfff0e
BhryanS2 <bhryanstepenhen@gmail.com>
2022-07-27 15:18:32 -0300

    feat: 🛠  terminando curso

--- javascript/explorando a linguagem/7.js
+++ javascript/explorando a linguagem/7.js
@@ -6,11 +6,25 @@ const listaDeDestinos = new Array(
   "Minas Gerais"
 );
 
-const idadeComprador = 15;
+const idadeComprador = 18;
+const estaAcompanhada = false;
+const temPassagemComprada = true;
 
-if (idadeComprador >= 18) {
-  console.log("comprou");
-  listaDeDestinos.splice(2, 1);
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+  console.log("Boa Viagem!!");
+  listaDeDestinos.splice(2, 1); //removendo item
 } else {
-  console.log("Menor de idade");
+  console.log("Não é maior de Idade e não posso vender");
 }
+
+console.log("Embarque: \n\n");
+if (idadeComprador >= 18 && temPassagemComprada) {
+  console.log("Boa viagem");
+} else {
+  console.log("Você não pode embarcar");
+}
+
+console.log(listaDeDestinos);
