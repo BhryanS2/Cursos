0f81bcf1ed0ebc1b1c889a7b52abecf1d27ed9dd
BhryanS2 <bhryanstepenhen@gmail.com>
2022-07-27 08:28:15 -0300

    templapte literal

---
+++ javascript/tipos, variáveis e funções/template literal.js
@@ -0,0 +1,7 @@
+const nome = "Bhryan";
+const idade = 17;
+
+const apresentacaoConcatenada =
+  "Meu nome é " + nome + " e tenho " + idade + " anos.";
+
+const apresentacaoTemplate = `Meu nome é ${nome} e tenho ${idade} anos.`;
