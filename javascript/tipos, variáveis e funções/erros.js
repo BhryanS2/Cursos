c1ddfb3cd29cff2606d073d9b7e48cbed08e6d52
BhryanS2 <bhryanstepenhen@gmail.com>
2022-07-27 07:55:59 -0300

    alguns erros

---
+++ javascript/tipos, variáveis e funções/erros.js
@@ -0,0 +1,10 @@
+// const constanteSemValor;
+// erro -> Missing initializer in const declaration
+
+// const numero = 1;
+
+// console.log(numero
+// erro ->  missing ) after argument list
+
+// console.log(variavelInesistente);
+// erro -> ReferenceError: variavelInesistente is not defined
