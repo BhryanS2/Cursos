8c03001c2721ce77ccf2f231db90f31aebc2eba2
BhryanS2 <bhryanstepenhen@gmail.com>
2022-07-27 13:43:17 -0300

    listas

---
+++ javascript/explorando a linguagem/6.js
@@ -0,0 +1,12 @@
+// listas
+const listaDeDestinos = new Array(
+  "Salvador",
+  "São Paulo",
+  "Rio de Janeiro",
+  "Minas Gerais"
+);
+
+console.table(listaDeDestinos);
+
+listaDeDestinos.push("Curitiba");
+console.table(listaDeDestinos);
