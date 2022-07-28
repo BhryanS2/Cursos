8cfe1fc959b1aa929fc6c0a57bb0beb2ba421d45
BhryanS2 <bhryanstepenhen@gmail.com>
2022-07-28 20:02:46 -0300

    feat: 🛠  Matriz

---
+++ javascript/arrays/matriz.js
@@ -0,0 +1,7 @@
+const alunos = ["Konan", "Pain", "Obito"];
+const notas = [8.5, 9.5, 7.5];
+
+const listaDeAlunos = [alunos, notas];
+
+console.log(listaDeAlunos[0][0]); // Konan
+console.log(listaDeAlunos[1][0]); // 8.5
