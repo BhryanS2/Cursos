4efd3296a647ba275f097a574639b2907b719580
BhryanS2 <bhryanstepenhen@gmail.com>
2022-07-29 07:41:38 -0300

    feat: 🛠  Percorrendo um array dentro de um objeto

---
+++ javascript/objetos/5.js
@@ -0,0 +1,8 @@
+const cliente = {
+  nome: "Luffy",
+  idade: 20,
+  cpf: "123.456.789-10",
+  arrayDeTelefone: ["(99) 99999-9999", "(88) 88888-8888", "(77) 7777-7777"],
+};
+
+cliente.arrayDeTelefone.forEach((tel) => console.log(tel));
