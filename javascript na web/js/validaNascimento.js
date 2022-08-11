5cef5c89aaac0832c879c280591098fdf55a5365
BhryanS2 <bhryanstepenhen@gmail.com>
2022-08-11 08:00:33 -0300

    refactor: ♻️ criando obj de erro

--- javascript na web/js/validaNascimento.js
+++ javascript na web/js/validaNascimento.js
@@ -2,11 +2,36 @@ const validadores = {
   dataNascimneto: (input) => validaDataDeNascimento(input),
 };
 
+const mensagensDeErro = {
+  nome: {
+    valueMissing: "O campo nome não pode estar vazio.",
+  },
+  email: {
+    valueMissing: "O campo email não pode estar vazio.",
+    typeMismatch: "O email digitado não é válido.",
+  },
+  senha: {
+    valueMissing: "O campo senha não pode estar vazio.",
+    patternMismatch:
+      "A senha deve conter 1 letra maiúscula, 1 letra minúscula, 1 número e 1 caracter especial e pelo menos 6 caracteres",
+  },
+  dataNascimento: {
+    valueMissing: "O campo data de nascimento não pode estar vazio.",
+    customError: "Você deve ser maior que 18 anos para se cadastrar",
+  },
+};
+
 export function valida(input) {
   const tipoDeInput = input.dataset.tipo;
 
   if (validadores[tipoDeInput]) {
     validadores[tipoDeInput](input);
+  }
+
+  if (input.validity.valid) {
+    input.parentElement.classList.remove("input-container--invalido");
+  } else {
+    input.parentElement.classList.add("input-container--invalido");
   }
 }
 
