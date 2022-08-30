755f901e3eba4f26e5a0aa676628cf37cf7ef72e
BhryanS2 <bhryanstepenhen@gmail.com>
2022-08-29 22:06:48 -0300

    Configurando novo projeto

---
+++ Vue-auth/auth-vue/src/views/NovoUsuario.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h1>Novo usuário</h1>
+    <form @submit.prevent="enviarFormulario">
+      <div class="form-group">
+        <label for="nome">Nome</label>
+        <input type="text" class="form-control" v-model="usuario.nome" />
+      </div>
+      <div class="form-group">
+        <label for="senha">Senha</label>
+        <input type="password" class="form-control" v-model="usuario.senha" />
+      </div>
+      <button class="btn btn-primary" type="submit">Salvar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      usuario: {
+        nome: "",
+        senha: "",
+        email: "",
+      },
+    };
+  },
+};
+</script>
