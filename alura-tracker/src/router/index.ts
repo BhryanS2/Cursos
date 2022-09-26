745e27832e6967a669c321fb9f1a8eda5796b1e6
BhryanS2 <bhryanstepenhen@gmail.com>
2022-09-26 16:25:43 -0300

    refactor: ♻️  extraindo view do formulario

--- alura-tracker/src/router/index.ts
+++ alura-tracker/src/router/index.ts
@@ -2,6 +2,7 @@ import { createWebHashHistory, createRouter, RouteRecordRaw } from "vue-router";
 
 import Tarefas from "../views/Tarefas.vue";
 import Projetos from "../views/Projetos.vue";
+import Formulario from "../views/Projetos/Formulario.vue";
 
 export const routes: RouteRecordRaw[] = [
   {
@@ -13,6 +14,17 @@ export const routes: RouteRecordRaw[] = [
     path: "/projetos",
     name: "projetos",
     component: Projetos,
+  },
+  {
+    path: "/projetos/novo",
+    name: "Novo projeto",
+    component: Formulario,
+  },
+
+  {
+    path: "/projetos/:id",
+    name: "Novo projeto",
+    component: Formulario,
   },
 ];
 
